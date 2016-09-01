7e985163404eaf4cc244072f484df10c1e979dbe
Mark Sims <markedwardsims@gmail.com>
2016-09-01 05:47:35 -0500

    reindexing the warriors

--- server.js
+++ server.js
@@ -17,12 +17,12 @@ var io = require('socket.io').listen(app.listen(3000, function() {
 
 
 var warriors = [
-    { id: 1231, name: 'Mr. T', 			wins: 0, image: 'images/mr-t.jpg' },
-    { id: 1232, name: 'Chuck Norris', 	wins: 0, image: 'images/chuck-norris.jpg' },
-    { id: 1233, name: 'Darth Vader', 	wins: 0, image: 'images/darth-vader.jpg' },
-    { id: 1234, name: 'Rocky Balboa', 	wins: 0, image: 'images/rocky-balboa.jpg' },
-    { id: 1235, name: 'T2000', 			wins: 0, image: 'images/t2000.jpg' },
-    { id: 1236, name: 'Nitro', 			wins: 0, image: 'images/nitro.jpg' }
+    { id: 1, name: 'Chuck Norris', 	wins: 0, image: 'images/chuck-norris.jpg' },
+    { id: 2, name: 'Mr. T', 		wins: 0, image: 'images/mr-t.jpg' },
+    { id: 3, name: 'Darth Vader', 	wins: 0, image: 'images/darth-vader.jpg' },
+    { id: 4, name: 'Rocky Balboa', 	wins: 0, image: 'images/rocky-balboa.jpg' },
+    { id: 5, name: 'T2000', 		wins: 0, image: 'images/t2000.jpg' },
+    { id: 6, name: 'Nitro', 		wins: 0, image: 'images/nitro.jpg' }
 ];
 
 io.on('connection', function(socket){
